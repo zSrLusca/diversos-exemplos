d706ae66b1ebc7f98ca0e227dfdda53275bc346d
zSrLusca <lucas.raniery81@gmail.com>
2025-11-16 17:12:57 -0300

    INIT: diversos exemplos react

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Exemplo01 from "./Exemplo01"
+
+export default function App() {
+  return (
+    <div>
+      <h1>Diversos exemplos de React - JS</h1>
+      <p>abaixo teremos diversos exemplos de react + JS</p>
+    
+    <Exemplo01/>
+    </div>
+  );
+}
---
+++ src/components/Exemplo01.jsx
@@ -0,0 +1,7 @@
+export default function Exemplo01(){
+
+
+    return(
+        <h2>Exemplo com </h2>
+    )
+}
