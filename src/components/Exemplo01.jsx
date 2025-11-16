751789b0233c218af6410562ce045e22973d7fff
zSrLusca <lucas.raniery81@gmail.com>
2025-11-16 17:49:36 -0300

    fix: exemplo01

--- src/components/Exemplo01.jsx
+++ src/components/Exemplo01.jsx
@@ -1,11 +1,14 @@
 export default function Exemplo01() {
   const Ligado = false;
-
-  if (Ligado === false) return <p>O computador esta desligado</p>;
+  let mensagem = "";
+  if (Ligado === false) {
+    mensagem = "O computador esta desligado";
+  }
 
   return (
     <div>
       <h2> Condicional Simples </h2>
+      <p>{mensagem}</p>
     </div>
   );
 }
